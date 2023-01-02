7e6a5ab32705741beaf4b51c3d133c7e21effaaa
Rusiru1998 <rusiruc21@gmail.com>
2023-01-02 13:57:46 +0530

    inital commit

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,31 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    id: 1,
+    firstName: "rusiru",
+    lastName: "nethmina",
+    age: 24,
+  },
+  {
+    id: 2,
+    firstName: "kosala",
+    lastName: "dayaratyna",
+    age: 22,
+  },
+];
+
 //all routes in here are starting with /users
-router.get('/', (req,res) => {
-    res.send('hello');
+router.get("/", (req, res) => {
+  //   console.log(users);
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(`User name is ${user.firstName}`);
 });
 
 export default router;
